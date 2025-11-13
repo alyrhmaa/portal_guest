6ecb029306dafce8d673eb7178d382cb8886735e
alyrhmaa <aliya24si@mahasiswa.pcr.ac.id>
2025-11-13 18:23:44 +0700

    kuiz 4

--- resources/views/layouts/guest/main.blade.php
+++ resources/views/layouts/guest/main.blade.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
 
+{{-- Start css ---}}
+@include('layouts.guest.css')
+{{-- End css --}}
+
+<body class="@yield('body-class', 'index-page')">
+
+{{-- Header --}}
+@include('layouts.guest.header')
+{{-- End Header --}}
+
+{{-- Main content --}}
+  <main id="main" style="margin-top: 100px;">
+    @yield('content')
+  </main>
+{{-- End Main content --}}
+{{-- Footer --}}
+@include('layouts.guest.footer')
+{{-- End Footer --}}
+
+{{-- Start JS --}}
+@include('layouts.guest.js')
+{{-- End JS --}}
+</body>
+</html>
